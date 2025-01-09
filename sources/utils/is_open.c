bf36243a1aad173dcc8ff36ca31c0a5c8b50154b
agerbaud <agerbaud@student.42lyon.fr>
2025-01-09 14:17:30 +0100

    fix is_open to compile

--- sources/utils/is_open.c
+++ sources/utils/is_open.c
@@ -6,13 +6,13 @@
 /*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 17:37:09 by agerbaud          #+#    #+#             */
-/*   Updated: 2025/01/08 17:38:40 by agerbaud         ###   ########.fr       */
+/*   Updated: 2025/01/09 14:17:05 by agerbaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-bool	check_open(char c)
+bool	is_open(char c)
 {
 	if (c == '0' || c == 'N' || c == 'S' || c == 'W' || c == 'E')
 		return (true);
